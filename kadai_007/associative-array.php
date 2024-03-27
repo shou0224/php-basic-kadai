16840d28840216c125c2293ec91ea8851c0ff8f4
shou0224 <131434884+shou0224@users.noreply.github.com>
2024-03-28 08:31:26 +0900

    kadai007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $yasai = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($yasai);
+    ?>
+  </p>
+</body>
+</html>
